dd5bb2ee9d9b027749a25a21e2a7530cb57048ec
orfneos <orfneos@yahoo.gr>
2026-05-28 23:21:03 +0300

    feat: add weather icons and round temperatures

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -10,7 +10,8 @@ const Forecast = ({ data }: Props) => {
     {data.list.filter((_: any, index: number) => index % 8 === 0).map((item: any) => (
       <div key={item.dt}>
         <p>{new Date(item.dt * 1000).toLocaleDateString('el-GR', {weekday: 'long'})}</p>
-        <p>{item.main.temp}°C</p>
+        <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} />
+        <p>{Number(item.main.temp).toFixed(1)}°C</p>
       </div>
     ))}
     </>
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -8,8 +8,9 @@ const WeatherCard = ({ data }: Props)  => {
 
   return(
     <>
+    <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" />
     <p>Όνομα Πόλης: {data.name}ς</p>
-    <p>Θερμοκρασία: {data.main.temp}°C</p>
+    <p>Θερμοκρασία: {Number(data.main.temp).toFixed(1)}°C</p>
     <p>Υγρασία: {data.main.humidity}%</p>
     <p>Περιγραφή: {data.weather[0].description}</p>
     </>
